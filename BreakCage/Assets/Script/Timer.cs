938db0a3fb762bb2a3ef4c804d85aaf1cb1b2917
cho <63055734+CHOYOOBUM@users.noreply.github.com>
2020-12-07 17:29:20 +0900

    엔딩지점 도착 시 타이머 스탑
    ..

--- BreakCage/Assets/Script/Timer.cs
+++ BreakCage/Assets/Script/Timer.cs
@@ -6,6 +6,7 @@ public class Timer : MonoBehaviour
 {
     public Text timerText;
     private float startTime;
+    private bool finished = false;
 
     void Start()
     {
@@ -15,11 +16,20 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (finished)
+            return;
+
         float t = Time.time - startTime;
 
         string minutes = ((int)t / 60).ToString();
         string seconds = (t % 60).ToString("f2");
 
         timerText.text = minutes + ":" + seconds;
+    }
+
+    public void FinishLine()
+    {
+        finished = true;
+        timerText.color = Color.yellow;
     }
 }
